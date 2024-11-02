75c551fa63a5792001dacad556e51ecefe278a53
L-ING <hlf01@icloud.com>
2024-11-02 13:51:27 +0800

    feat: add all args

--- forecast_train.py
+++ forecast_train.py
@@ -6,18 +6,6 @@ if not "Informer" in sys.path:
 from Informer.exp.exp_informer import Exp_Informer as Exp
 import torch
 from config import args, setting
-import argparse
-
-arg_parser = argparse.ArgumentParser()
-
-arg_parser.add_argument("--train_epochs", type=int, default=6)
-arg_parser.add_argument("--lradj", type=str, default="type1")
-arg_parser.add_argument("--patience", type=int, default=20)
-
-parsed_args = arg_parser.parse_args()
-
-for key, value in vars(parsed_args).items():
-    setattr(args, key, value)
 
 print("Args in experiment:")
 print(args)
